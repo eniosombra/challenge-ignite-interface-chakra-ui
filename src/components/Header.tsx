2b846730b6ea1c72aa642c44aba540a03f30f036
Enio L. Sombra <eniosombra@gmail.com>
2021-06-03 12:00:16 -0300

    Tornando o componente responsivo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,32 @@
-import {
-  Heading,
-  Avatar,
-  Flex,
-  Text,
-  Image,
-  Stack,
-  Box,
-  Center
-} from '@chakra-ui/react'
+import { Heading, Flex, Text, Box } from '@chakra-ui/react'
 
 export function Header() {
   return (
     <>
-      <Box>
-        <Image src="/images/banner.svg" alt="banner" w="100%" />
-        <Box pos="absolute" top={['2', '4', '16']} left={['5', '12', '90']}>
+      <Flex
+        bgImage={'images/banner.svg'}
+        bgSize="cover"
+        bgRepeat="no-repeat"
+        bgPosition="center"
+        align="center"
+        h={[, 160, 240, 320, 380, 460]}
+      >
+        <Box mx={[, 4, 8, 16, 32]} maxW={[, 300, 460, 600]}>
           <Heading
             color="app.title"
-            fontWeight="medium"
-            fontSize={[15, 24, 36]}
+            fontWeight="normal"
+            fontSize={[, 24, 32, 36]}
+            mb={4}
           >
             5 Continentes, <br />
             infinitas possibilidades.
           </Heading>
-          <Text mt={[0, 2, 4]} fontSize={[10, 10, 20]}>
-            Chegou a hora de tirar do papel a viagem que você <br /> sempre
-            sonhou.
+
+          <Text color="app.info" fontSize={[, 12, 16, 20]} mb={4}>
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
           </Text>
         </Box>
-      </Box>
+      </Flex>
     </>
   )
 }
