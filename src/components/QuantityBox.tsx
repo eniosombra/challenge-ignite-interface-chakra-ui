9ed3d9c70a0c8189b0237af5a9cca5b24a7fc479
Enio L. Sombra <eniosombra@gmail.com>
2021-06-04 08:04:19 -0300

    componente de quantitativos relacionados ao continente

---
+++ src/components/QuantityBox.tsx
@@ -0,0 +1,18 @@
+import { GridItem, Text } from '@chakra-ui/react'
+
+type QuantityBoxProps = {
+  quantity: number
+  label: string
+}
+export function QuantityBox({ quantity, label }: QuantityBoxProps) {
+  return (
+    <GridItem>
+      <Text fontSize={48} fontWeight="semibold" color="app.highlight">
+        {quantity}
+      </Text>
+      <Text fontSize={24} fontWeight="semibold">
+        {label}
+      </Text>
+    </GridItem>
+  )
+}
