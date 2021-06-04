8764883d443d18f737faf81ed2a401aca662e21d
Enio L. Sombra <eniosombra@gmail.com>
2021-06-04 16:05:24 -0300

    criado api para listar os continentes

---
+++ src/pages/api/continents.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import continents from '../../assets/continents.json'
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  return response.json(continents)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
+import { GetServerSideProps } from 'next'
 import { Header } from '../components/Header'
 import { Menu } from '../components/Menu'
 import { Carousel } from '../components/Carousel'
+import { api } from '../services/api'
 
-export default function Home() {
+export default function Home({ continents }) {
   return (
     <>
       <Header />
       <Menu />
-      <Carousel />
+      <Carousel continents={continents} />
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await api.get('/continents')
+
+  return {
+    props: {
+      continents: data
+    }
+  }
 }
