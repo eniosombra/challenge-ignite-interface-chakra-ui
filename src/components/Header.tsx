95c0555c4e5eedc39b5e8fe08c0b86f511dd06ad
Enio L. Sombra <eniosombra@gmail.com>
2021-06-07 15:52:41 -0300

    ajustes no componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Heading, Flex, Text, Box, Image, Center } from '@chakra-ui/react'
 
 export function Header() {
   return (
-    <>
+    <Box mb={[, 8, 16, 24]}>
       <Center>
         <Image src="/images/logo.svg" p={3} />
       </Center>
@@ -15,22 +15,44 @@ export function Header() {
         align="center"
         h={[, 160, 240, 320, 380, 460]}
       >
-        <Box mx={[, 4, 8, 16, 32]} w={[, 300, 460, 600]}>
-          <Heading
-            color="app.title"
-            fontWeight="normal"
-            fontSize={[, 24, 32, 36]}
-            mb={4}
-          >
-            5 Continentes, <br />
-            infinitas possibilidades.
-          </Heading>
+        <Flex
+          mx={[, 4, 8, 16, 32]}
+          align="center"
+          justify="space-between"
+          w="100%"
+        >
+          <Box>
+            <Heading
+              color="app.title"
+              fontWeight="normal"
+              fontSize={[, 24, 32, 36]}
+              mb={4}
+            >
+              5 Continentes, <br />
+              infinitas possibilidades.
+            </Heading>
 
-          <Text color="app.info" fontSize={[, 12, 16, 20]} mb={4}>
-            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
-          </Text>
-        </Box>
+            <Text color="app.info" fontSize={[, 12, 16, 20]} mb={4}>
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+            </Text>
+          </Box>
+
+          <Image
+            src="/images/airplane.svg"
+            alt="airplane"
+            w={[, , '400px', '460px', '540px', '640px']}
+            display={['none', 'none', 'block']}
+            transform={[
+              ,
+              ,
+              'translateY(12px)',
+              'translateY(40px)',
+              'translateY(55px)',
+              'translateY(60px)'
+            ]}
+          />
+        </Flex>
       </Flex>
-    </>
+    </Box>
   )
 }
