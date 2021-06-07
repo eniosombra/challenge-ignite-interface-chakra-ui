6b245d113efa6963db6b000d80ce24f820deac9f
Enio L. Sombra <eniosombra@gmail.com>
2021-06-07 16:32:06 -0300

    ajuste no componente menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,18 @@
-import { GridItem, Image, SimpleGrid, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  GridItem,
+  Image,
+  SimpleGrid,
+  Text,
+  useBreakpointValue
+} from '@chakra-ui/react'
 
 export function Menu() {
+  const isMobile = useBreakpointValue({
+    base: false,
+    sm: true
+  })
   const menus = [
     { name: 'vida noturna', image: 'night.svg' },
     { name: 'praia', image: 'beach.svg' },
@@ -13,10 +25,21 @@ export function Menu() {
     <SimpleGrid columns={[null, 2, 3, 5]} spacing={5}>
       {menus.map(menu => (
         <GridItem key={menu.name} align="center">
-          <Image src={`/images/${menu.image}`} alt={menu.name} />
-          <Text mt={5} fontWeight="semibold" fontSize={[null, '18', '24']}>
-            {menu.name}
-          </Text>
+          {isMobile ? (
+            <>
+              <Image src={`/images/${menu.image}`} alt={menu.name} />
+              <Text mt={5} fontWeight="semibold" fontSize={[null, '18', '24']}>
+                {menu.name}
+              </Text>
+            </>
+          ) : (
+            <Flex align="center" justify="center" mt={4}>
+              <Box w={3} h={3} bg="app.highlight" borderRadius="100%" />
+              <Text ml={4} fontWeight="semibold" fontSize={[null, '18', '24']}>
+                {menu.name}
+              </Text>
+            </Flex>
+          )}
         </GridItem>
       ))}
     </SimpleGrid>
